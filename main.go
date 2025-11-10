90de43d5d4593d126d58203608617ae2c7365741
CarlosM <carlosandresmartinezortiz632@gmail.com>
2025-11-09 21:35:45 -0500

    feat: add database connection and update dependencies in go.mod

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 
 	"github.com/gorilla/mux"
 
+	"github.com/carloss765/RestApiGo/db"
 	"github.com/carloss765/RestApiGo/routes"
 )
 
-
-
 func main() {
+
+	db.DBConnection()
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
