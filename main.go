3cce991ea4729ca4a1885608c433c868794a9fcf
CarlosM <carlosandresmartinezortiz632@gmail.com>
2025-11-09 21:04:22 -0500

    feat: add configuration for Air, refactor main handler to use routes

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+
+	"github.com/carloss765/RestApiGo/routes"
 )
+
+
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	})
+	r.HandleFunc("/", routes.HomeHandler)
 
 	http.ListenAndServe(":3000", r)
 }
