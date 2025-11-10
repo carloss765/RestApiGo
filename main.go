702263a8032fa598861add68352b45209a246899
CarlosM <carlosandresmartinezortiz632@gmail.com>
2025-11-09 21:51:48 -0500

    feat: add User and Task models with database migration in main.go

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/carloss765/RestApiGo/db"
+	"github.com/carloss765/RestApiGo/models"
 	"github.com/carloss765/RestApiGo/routes"
 )
 
 func main() {
 
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.User{})
+	db.DB.AutoMigrate(models.Task{})
 
 	r := mux.NewRouter()
 
